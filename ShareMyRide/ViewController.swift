d7997f7ed57a7df518209a0d84f722838741e12a
Dennis Adjei-Baah <dadjeibaah@gmail.com>
2016-02-17 23:47:48 -0500

    Initial Commit

---
+++ ShareMyRide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareMyRide
+//
+//  Created by Dennis Adjei-Baah on 2/17/16.
+//  Copyright © 2016 Dennis Adjei-Baah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
