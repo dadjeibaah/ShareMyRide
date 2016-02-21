ede6c64fb3875dc5284d81b1f16e7c60647f6cdb
Dennis Adjei-Baah <dadjeibaah@gmail.com>
2016-02-20 19:16:37 -0500

    adding gitignore

---
+++ ShareMyRide/TopPlacesRepository.swift
@@ -0,0 +1,9 @@
+//
+//  TopPlacesRepository.swift
+//  ShareMyRide
+//
+//  Created by Dennis Adjei-Baah on 2/19/16.
+//  Copyright © 2016 Dennis Adjei-Baah. All rights reserved.
+//
+
+import Foundation
