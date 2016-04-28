ae19139463b2f601a1b9292558fd28dda8389905
Dennis Adjei-Baah <dadjeibaah@gmail.com>
2016-04-28 00:53:58 -0400

    adds test for rides model

---
+++ ShareMyRideTests/RideTests.swift
@@ -0,0 +1,16 @@
+//
+//  RideTests.swift
+//  ShareMyRide
+//
+//  Created by Dennis Adjei-Baah on 4/28/16.
+//  Copyright © 2016 Dennis Adjei-Baah. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import ShareMyRide
+
+class RidesTests:XCTestCase{
+    
+}
